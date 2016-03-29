373f0d70dc28e3e0d7c15f65779a2e116a6ce9a8
Kirk Pettinga <kirk.pettinga@gmail.com>
2016-03-29 12:58:55 +0700

    add self-hosted fonts

--- header.php
+++ header.php
@@ -8,7 +8,6 @@
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<?php wp_head(); ?>
-    <link rel="stylesheet" type="text/css" href="https://cloud.typography.com/6710674/7917352/css/fonts.css" />
 </head>
 <body <?php body_class(); ?>>
 
